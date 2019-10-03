6adb58769ee06c88f3e460cabfa2e20412a98ef9
Oleg Boiko <oleh.boiko@gmail.com>
2019-10-02 20:33:23 -0700

    Wild sgd

--- sgd.py
+++ sgd.py
@@ -85,8 +85,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
     for iter in range(start_iter + 1, iterations + 1):
         # You might want to print the progress every few iterations.
 
-        loss = None
         ### YOUR CODE HERE
+        loss, grad = f(x)
+        x -= step*grad
         ### END YOUR CODE
 
         x = postprocessing(x)
